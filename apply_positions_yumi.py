bd94620fa44d6d7d46e6c332a5d87c1c986902e2
ronberenstein <ron.berenstein@berkeley.edu>
2018-08-14 19:21:55 -0700

    bug fix

--- apply_positions_yumi.py
+++ apply_positions_yumi.py
@@ -17,7 +17,7 @@ stop_btn = 0
 
 def disconnect_tool(pos_num):
     global stop_btn
-    while True:
+    while stop_btn==0:
         pygame.event.pump()
         btn_y = pygame.joystick.Joystick(0).get_button(4)
         stop_btn = pygame.joystick.Joystick(0).get_button(0)
@@ -31,14 +31,15 @@ def disconnect_tool(pos_num):
     pos.from_frame = 'robot'
 
     h0 = 0.1453
-    h1 = 0.097
+    h1 = 0.094
 
     x0 = 0.394  # y = -0.4303
     x1 = 0.4967  # y = -0.4324
     x2 = 0.5533  # y = -0.4332
     x3 = x2
     x4 = 0.4949  # y = -0.4353
-    x5 = 0.394
+    x5 = 0.489
+    x6 = 0.394
 
     y = -0.4365
 
@@ -50,16 +51,18 @@ def disconnect_tool(pos_num):
     v = 200
 
     if pos_num == 0:
-        X, Y, H, ROT, V = x5, y, h1, rot1, v
-    elif pos_num == 1:
-        X, Y, H, ROT, V = x4, y, h1, rot1, v_very_slow
+        X, Y, H, ROT, V = x6, y, h1, rot1, v
+    elif pos_num == 1:#NEW point
+        X, Y, H, ROT, V = x5, y, 0.0964, rot1, v
     elif pos_num == 2:
-        X, Y, H, ROT, V = x3, y, h1, rot1, v_very_slow
+        X, Y, H, ROT, V = x4, y, h1, rot1, v_very_slow
     elif pos_num == 3:
-        X, Y, H, ROT, V = x2, y, h0, rot1, v_slow
+        X, Y, H, ROT, V = x3, y, h1, rot1, v_very_slow
     elif pos_num == 4:
-        X, Y, H, ROT, V = x1, y, h0, rot1, v_slow
+        X, Y, H, ROT, V = x2, y, h0, rot1, v_slow
     elif pos_num == 5:
+        X, Y, H, ROT, V = x1, y, h0, rot1, v_slow
+    elif pos_num == 6:
         X, Y, H, ROT, V = x0, y, h0, rot1, v
 
     pos.translation = X, Y, H
@@ -79,12 +82,6 @@ def connect_tool(pos_num):
         stop_btn = pygame.joystick.Joystick(0).get_button(0)
         if btn_y == 1:
             break
-            # connect_tool(pos_num)
-            # pos_num = pos_num + 1
-        # if pos_num == 6:
-        #     pos_num = 0
-        # if btn_a == 1:
-        #     break
 
     X = 0
     Y = 0
@@ -140,15 +137,16 @@ def connect_tool(pos_num):
 
 def main():
     global stop_btn
-    pos_num = 0
     try:
-    	while stop_btn==0:
+        while stop_btn==0:
             for i in range(0,6,1):
-            	connect_tool(i)
+                connect_tool(i)
+            if stop_btn==1:
+                continue
+            for i in range(0, 5, 1):
+                disconnect_tool(i)
     except:
-	pass
-        #for i in range(0, 5, 1):
-            #disconnect_tool(i)
+        pass
 
     yumi.stop()
 
